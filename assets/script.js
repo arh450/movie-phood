2fd8cfb9ae645845dbf8c1fb45ca26bd6fb3ee54
hank amadeus <hankamadeus@email.com>
2020-05-07 22:06:55 -0400

    Wrote some pseudo code for function and operation of project

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// 1. Link material css & js accordingly
+
+// START PAGE
+// Contd a alert asking for location is also shown to user. (pending)
+// User is presented a page that displays name, decription, and a button that when clicked brings user to --->
+
+// QUESTIONS & GET TO KNOW USER
+// A card with a question title, first being if the user is over 21-->
+// If the user is over 21 provide alcholic drink choices, 
+// Else provide non-alcholic choice (using cocktailDB api calls).<--
+
+// The same card with another question title (related to MOVIES) is then presented... find parameter to get movie GENRES (to-do).
+// EXAMPLE
+// Question: Which of the following movie genres to you enjoy
+// A. Action
+// B. Sci-Fi
+// C. Horror
+// D. Comedy
+
+// The same card with another question title (related to FOOD) is then presented
+// BASE QUESTION ON TYPE OF FOOD CATEGORY https://www.themealdb.com/api/json/v1/1/list.php?c=list
+
+// The same card with another question title (related to DRINKS) is then presented
+// BASE QUESTION ON TYPE OF DRINK CATEGORY https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list
+
+
+
+
+
+
+
