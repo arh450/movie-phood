215aca8f44eb79a8c4e8e0fa1360fc0b050966d4
hank amadeus <hankamadeus@email.com>
2020-05-09 14:37:42 -0400

    created movie info object that will store random movie object items (title, poster, plot, overview)

--- assets/js/moviescript.js
+++ assets/js/moviescript.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
+
+    // Empty object that stores users random movie recommendation information
+
+    var userMovieInfo = {
+        title: "",
+        year: "",
+        imgSrc: "",
+        plot: ""
+    }
+
     // Based on what the user inputed for a movie(s) in the movie-user-input from the moviesection.html
 
     // on click of #movie-submit button (shown as submit on HTML)
@@ -22,7 +32,7 @@ $(document).ready(function () {
         // console.log(randomUserMovieInput);
 
         // Name of random movie from the userMovieInputsArray
-        console.log(userMovieInputs[randomUserMovieInput])
+        // console.log(userMovieInputs[randomUserMovieInput])
 
         // Themoviedb API Key
         var movieApiKey = "2938a7e57a5497d72c32f5b9a3fba36f";
@@ -50,12 +60,27 @@ $(document).ready(function () {
                 var randomMovieResult = Math.floor(Math.random() * idResponse.results.length);
                 // console.log(randomMovieResult);
 
-                // Varaible that takes that randomMovieResult number and gets the title of the randomized movie title
-                var finalMovieTitle = idResponse.results[randomMovieResult].title;
-                // console.log(finalMovieTitle);
+                // Varaible that takes the randomMovieResult number (index position) and then returns the object of the randomized movie
+                var recMovieResult = idResponse.results[randomMovieResult];
+                console.log(recMovieResult);
+
+                // Variable that gives release date of recomended (random movie)
+                var recMovieYear = recMovieResult.release_date;
+                // Variable that gives recommended (random) movie poster using theMovieDB image url
+                var recMoviePoster = `http://image.tmdb.org/t/p/w200${recMovieResult.poster_path}`;
+
+                // Set empty userMovieInfo object with recommended random movie result data
+                userMovieInfo = {
+                    title: recMovieResult.title,
+                    year: recMovieYear,
+                    imgSrc: recMoviePoster,
+                    plot: recMovieResult.overview
+                }
+
+                console.log(userMovieInfo);
 
                 // For test purposes to display to page
-                $("#test-random-movie").text(`You should check out: ${finalMovieTitle}`);
+                // $("#test-random-movie").text(`You should check out: ${finalMovieTitle}`);
             });
         });
     });
