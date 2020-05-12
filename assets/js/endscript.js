477b0daefc01abcaa4847f596da0ca98991ed163
hank amadeus <hankamadeus@email.com>
2020-05-12 19:17:45 -0400

    added results.html and linked css and script from index, created endscript.js and coped functions from local test folder, added localstorage capabilities to user movie/food/drink objects to set and get the information to be displayed on html

---
+++ assets/js/endscript.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    function renderRestart(html) {
+        window.location.replace("index.html");
+    }
+
+    function getMovieCombo() {
+        var getMovieInfo = localStorage.getItem('MovieInfo');
+        var finalMovieInfo = JSON.parse(getMovieInfo);
+
+        console.log(finalMovieInfo);
+
+        $("#movie-poster").attr("src", finalMovieInfo.imgSrc);
+        $("#movie-title").text(finalMovieInfo.title);
+        $("#movie-year").text(`Year: ${finalMovieInfo.year}`);
+        $("#movie-plot").text(finalMovieInfo.plot);
+    }
+
+    function getFoodCombo() {
+        var getFoodInfo = localStorage.getItem('FoodInfo');
+        var finalFoodInfo = JSON.parse(getFoodInfo);
+
+        $("#food-poster").attr("src", finalFoodInfo.foodimgSrc);
+        $("#food-title").text(finalFoodInfo.meal);
+        $("#food-recipe").attr("href", finalFoodInfo.foodRecipe);
+        console.log(finalFoodInfo);
+
+
+    }
+
+    function getDrinkCombo() {
+        var getDrinkInfo = localStorage.getItem('DrinkInfo');
+        var finalDrinkInfo = JSON.parse(getDrinkInfo);
+
+
+        console.log(finalDrinkInfo);
+
+
+    }
+
+    getMovieCombo();
+    getFoodCombo();
+    getDrinkCombo()
+
+
+    $(document).on("click", "#thumb-down-btn", function (event) {
+        renderRestart("index.html");
+    });
+
+    $(document).on("click", "#thumb-up-btn", function (event) {
+
+    });
+
+    $(document).on("click", "#home-btn", function (event) {
+        renderRestart("index.html");
+    });
+});
