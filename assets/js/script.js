fe3131fa78795fa3590ee2fcf29dfd1ccc86caa3
hank amadeus <hankamadeus@email.com>
2020-05-12 17:45:03 -0400

    wrote on click of drink submit to console random drink results

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,11 @@ $(document).ready(function () {
       fRecipe: ""
     },
 
-
+    userDrinkInfo: {
+      dTitle: "",
+      dImgSrc: "",
+      dIns: ""
+    }
 
 
   }
@@ -193,29 +197,29 @@ $(document).ready(function () {
       // theMealDB to filter users selected food category
       var filterFoodURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${userFoodCategory}`;
       // AJAX call to get filtered response
-      $.get(filterFoodURL).then(function (filteredResponse) {
-        // console.log(filteredResponse);
+      $.get(filterFoodURL).then(function (filteredFoodResponse) {
+        // console.log(filteredFoodResponse);
 
-        // Varialbe similar to randomUserMovieInput, that generates a random index position from the filteredResponse
-        var randomFoodResult = Math.floor(Math.random() * filteredResponse.meals.length);
+        // Varialbe similar to randomUserMovieInput, that generates a random index position from the filteredFoodResponse
+        var randomFoodResult = Math.floor(Math.random() * filteredFoodResponse.meals.length);
         // console.log(randomFoodResult);
 
-        // Variable that takes the randomFoodResult (number/index position) and matches within the filteredResponse array of objects and returns the meal title
-        var recFoodTitle = filteredResponse.meals[randomFoodResult].strMeal;
+        // Variable that takes the randomFoodResult (number/index position) and matches within the filteredFoodResponse array of objects and returns the meal title
+        var recFoodTitle = filteredFoodResponse.meals[randomFoodResult].strMeal;
         // console.log(recFoodTitle);
 
         // Variable similar to recFoodTitle, that instead returning food title (.strMeal), it returns the food's image path
-        var recFoodImage = filteredResponse.meals[randomFoodResult].strMealThumb;
+        var recFoodImage = filteredFoodResponse.meals[randomFoodResult].strMealThumb;
         // console.log(recFoodImage);
 
         // Last AJAX call regarding food, this searches the recFoodTitle (food name) and is used to get the recipe source for the food
         var foodSearchURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recFoodTitle}`;
 
-        $.get(foodSearchURL).then(function (searchResponse) {
-          // console.log(searchResponse);
+        $.get(foodSearchURL).then(function (searchFoodResponse) {
+          // console.log(searchFoodResponse);
 
           // Variable to return the recipe source (URL) from the searchResponse
-          var recFoodRecipe = searchResponse.meals[0].strSource;
+          var recFoodRecipe = searchFoodResponse.meals[0].strSource;
           // console.log(recFoodRecipe);
 
           userFoodInfo = {
@@ -231,6 +235,58 @@ $(document).ready(function () {
       });
       renderHtml("drinksection.html");
       populateDrinkSelect()
+    }
+  });
+
+  // on click of #drink-submit button (shown as submit on HTML)
+  $(document).on("click", "#drink-submit", function (event) {
+    // variable to show what user selected for drink category
+    var userDrinkCategory = $("#drink-category-select").val();
+    console.log(userDrinkCategory);
+
+    // if statement to prevent user from leaving drink select blank
+    if (userDrinkCategory === null) {
+      alert("please select a drink category");
+    } else {
+      // theCockTailDB to filter the users selected drink category
+      var filterDrinkURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${userDrinkCategory}`;
+      // AJAX call to get filtered drink response
+      $.get(filterDrinkURL).then(function (filteredDrinkResponse) {
+        // console.log(filteredDrinkResponse);
+
+        // Varialbe similar to other random inputs, generates a random index position from the filteredDrinkResponse
+        var randomDrinkResult = Math.floor(Math.random() * filteredDrinkResponse.drinks.length);
+        console.log(randomDrinkResult);
+
+        // Variable that takes the randomDrinkResult (number/index position) and matches within the filteredDrinkResponse array of objects and returns the drink title
+        var recDrinkTitle = filteredDrinkResponse.drinks[randomDrinkResult].strDrink;
+        console.log(recDrinkTitle);
+
+        // Variable similar to recDrinkTitle, that instead of returning food title (.strDrink), it returns the food's image path
+        var recDrinkImage = filteredDrinkResponse.drinks[randomDrinkResult].strDrinkThumb;
+        console.log(recDrinkImage);
+
+        // Last AJAX call regarding drinks, this searches the recDrinkTitle (drink name) and is used to get the instructions for the drink
+        var drinkSearchURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${recDrinkTitle}`;
+
+        $.get(drinkSearchURL).then(function (searchDrinkResponse) {
+          // console.log(searchDrinkResponse);
+
+          // Variable to return the recipe source (URL) from the searchDrinkResponse
+          var recDrinkIns = searchDrinkResponse.drinks[0].strInstructions;
+          // console.log(recDrinkIns);
+
+          userDrinkInfo = {
+            dTitle: recDrinkTitle,
+            dImgSrc: recDrinkImage,
+            dIns: recDrinkIns
+          }
+
+          console.log(userDrinkInfo);
+
+        });
+      });
+
     }
   });
 
